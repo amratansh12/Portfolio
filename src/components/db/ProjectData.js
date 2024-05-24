c39e060a4b41a5ed5f575e716d5c4a1a8e25663c
amratansh12 <ashri1205@gmail.com>
2024-05-24 12:16:39 +0530

    Updating skills and projects

--- src/components/db/ProjectData.js
+++ src/components/db/ProjectData.js
@@ -1,43 +1,48 @@
 const ProjectData = [
-    {
-        id: "1",
-        name: "Chat Application",
-        description: "A real-time chat application built using the MERN stack and Socket.io for seamless communication and interactive user experiences.",
-        link1: "https://github.com/amratansh12/Chat-App-Api",
-        link2: "https://github.com/amratansh12/Chat-App"
-    },
-    {
-        id: "2",
-        name: "URL Shortener",
-        description: "A URL Shortener application developed with the MERN stack and RESTful APIs for efficient link management and streamlined web navigation.",
-        link1: "https://github.com/amratansh12/URL-Shortener-Api",
-        link2: "https://github.com/amratansh12/URL-Shortener",
-    },
-    {
-        id: "3",
-        name: "Face Recognition App",
-        description: "A Face Recognition application constructed with the PERN stack and RESTful APIs for accurate facial detection and identification.",
-        link1: "https://github.com/amratansh12/Face-recognition-app-API",
-        link2: "https://github.com/amratansh12/Face-recognition-app",
-    },
-    {
-        id: "4",
-        name: "Quiz App",
-        description: "A Quiz application built with React, Firebase, and RESTful APIs for interactive quizzes, real-time data storage, and seamless user engagement.",
-        link1: "https://github.com/amratansh12/Quiz-App-IMG",
-    },
-    {
-        id: "5",
-        name: "Radio Telescope Project",
-        description: "A Radio Telescope website crafted using React with a team of 5 developers to deliver an immersive platform for PaAC club's astronomical endeavors.",
-        link1: "https://github.com/amratansh12/RTP-Project",
-    },
-    {
-        id: "6",
-        name: "See More on GitHub...",
-        description: "View more of my projects and work on my GitHub...",
-        link1: "https://github.com/amratansh12?tab=repositories",
-    },
-]
+  {
+    id: "1",
+    name: "Medical Store",
+    description:
+      "An e-commerce store to buy medicines and manage stores built using MERN stack, Zustand and Restful APIs for integrating maps.",
+    link1: "https://github.com/amratansh12/Techshila-2024",
+  },
+  {
+    id: "2",
+    name: "Discord Clone",
+    description:
+      "A real-time server-based chatting and calling application built using NextJS, sockets.io, PrismaORM, TailwindCSS and WebRTC",
+    link1: "https://github.com/amratansh12/slack-clone",
+  },
+  {
+    id: "3",
+    name: "Chat Application",
+    description:
+      "A real-time chat application built using the MERN stack, BootStrap and Socket.io for seamless communication and interactive user experiences.",
+    link1: "https://github.com/amratansh12/Chat-App-Api",
+    link2: "https://github.com/amratansh12/Chat-App",
+  },
+  {
+    id: "4",
+    name: "URL Shortener",
+    description:
+      "A URL Shortener application developed with the MERN stack and RESTful APIs for efficient link management and streamlined web navigation.",
+    link1: "https://github.com/amratansh12/URL-Shortener-Api",
+    link2: "https://github.com/amratansh12/URL-Shortener",
+  },
+  {
+    id: "5",
+    name: "Face Recognition App",
+    description:
+      "A Face Recognition application constructed with the PERN stack and RESTful APIs for accurate facial detection and identification.",
+    link1: "https://github.com/amratansh12/Face-recognition-app-API",
+    link2: "https://github.com/amratansh12/Face-recognition-app",
+  },
+  {
+    id: "6",
+    name: "See More on GitHub...",
+    description: "View more of my projects and work on my GitHub...",
+    link1: "https://github.com/amratansh12?tab=repositories",
+  },
+];
 
 export default ProjectData;
--- src/components/db/SkillData.js
+++ src/components/db/SkillData.js
@@ -1,92 +1,104 @@
 const SkillData = [
-    {
-        id: "1",
-        type: 'Frontend',
-        skills:[
-            {
-                name: "HTML5",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/html-icon.png"
-            },
-            {
-                name: "CSS3",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/css-icon.png"
-            },
-            {
-                name: "JavaScript",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/javascript-programming-language-icon.png"
-            },
-            {
-                name: "TypeScript",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/typescript-programming-language-icon.png"
-            },
-            {
-                name: "ReactJS",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/react-js-icon.png"
-            },
-            {
-                name: "NextJS",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/nextjs-icon.png"
-            },
-        ]
-    },
-    {
-        id: "2",
-        type: 'Backend',
-        skills:[
-            {
-                name: "JavaScript",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/javascript-programming-language-icon.png"
-            },
-            {
-                name: "TypeScript",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/typescript-programming-language-icon.png"
-            },
-            {
-                name: "Node.JS",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/node-js-icon.png"
-            },
-            {
-                name: "Express.JS",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/express-js-icon.png"
-            },
-        ]
-    },
-    {
-        id: "3",
-        type: 'Devops',
-        skills:[
-            {
-                name: "Git/GitHub",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-icon.png"
-            },
-            {
-                name: "Docker",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/docker-icon.png"
-            },
-            {
-                name: "Jenkins",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/jenkins-icon.png"
-            },
-            {
-                name: "AWS",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/aws-icon.png"
-            },
-        ]
-    },
-    {
-        id: "4",
-        type: 'Databases',
-        skills: [
-            {
-                name: "MongoDB",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/mongodb-icon.png"
-            }, 
-            {
-                name: "PostgreSQL",
-                icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postgresql-icon.png"
-            }
-        ]
-    }
-]
+  {
+    id: "1",
+    type: "Frontend",
+    skills: [
+      {
+        name: "HTML5",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/html-icon.png",
+      },
+      {
+        name: "CSS3",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/css-icon.png",
+      },
+      {
+        name: "JavaScript",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/javascript-programming-language-icon.png",
+      },
+      {
+        name: "TypeScript",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/typescript-programming-language-icon.png",
+      },
+      {
+        name: "ReactJS",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/react-js-icon.png",
+      },
+      {
+        name: "NextJS",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/nextjs-icon.png",
+      },
+    ],
+  },
+  {
+    id: "2",
+    type: "Backend",
+    skills: [
+      {
+        name: "JavaScript",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/javascript-programming-language-icon.png",
+      },
+      {
+        name: "TypeScript",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/typescript-programming-language-icon.png",
+      },
+      {
+        name: "Node.JS",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/node-js-icon.png",
+      },
+      {
+        name: "Express.JS",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/express-js-icon.png",
+      },
+    ],
+  },
+  {
+    id: "3",
+    type: "Tools",
+    skills: [
+      {
+        name: "Git/GitHub",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-icon.png",
+      },
+      {
+        name: "Docker",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/docker-icon.png",
+      },
+      {
+        name: "TailwindCSS",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/tailwind-css-icon.png",
+      },
+      {
+        name: "Zustand",
+        icon: "https://repository-images.githubusercontent.com/180328715/fca49300-e7f1-11ea-9f51-cfd949b31560",
+      },
+      {
+        name: "WebRTC",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/webrtc-icon.png",
+      },
+      {
+        name: "RestAPIs",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/rest-api-icon.png",
+      },
+    ],
+  },
+  {
+    id: "4",
+    type: "Databases",
+    skills: [
+      {
+        name: "MongoDB",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/mongodb-icon.png",
+      },
+      {
+        name: "PostgreSQL",
+        icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postgresql-icon.png",
+      },
+      {
+        name: "PrismaORM",
+        icon: "https://static-00.iconduck.com/assets.00/prisma-icon-423x512-isxs6996.png",
+      },
+    ],
+  },
+];
 
 export default SkillData;
